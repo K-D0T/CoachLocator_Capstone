b202c0bb90449d8beecf1b5e5ebb0227022962dd
unknown <kaiden.thrailkill@gmail.com>
2024-01-31 15:26:26 -0500

    Created add coach page and working form. Also added a base page to hold navbar. Fixed Views to be less repetitive

--- home/forms.py
+++ home/forms.py
@@ -1,6 +1,29 @@
 from django import forms
+from .models import Coaches
 
 class ZipSearchForm(forms.Form):
     zip_code = forms.CharField(max_length=10, label='Enter your zip code', required=True)
+
+
+class CoachForm(forms.ModelForm):
+
+
+    class Meta:
+        model = Coaches
+        fields = '__all__'
+
+        widgets = {
+
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for i in self.fields:
+
+            self.fields[i].widget.attrs['class'] = 'form-control'
+
+            self.fields[i].widget.attrs['required'] = True
+
 
     
--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from django.contrib import admin
 from . import views
 
+app_name = 'home'
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('nearme', views.nearme, name='nearme')
+    path('nearme', views.nearme, name='nearme'),
+    path('becomecoach', views.becomecoach, name='becomecoach'),
 ]

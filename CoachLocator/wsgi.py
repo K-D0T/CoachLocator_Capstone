88c26a0fd84efb4de03d40ec1c234d2d8be5168e
unknown <kaiden.thrailkill@gmail.com>
2024-04-21 16:34:36 -0400

    testing database connection

--- CoachLocator/wsgi.py
+++ CoachLocator/wsgi.py
@@ -1,10 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-
-
 settings_module = "CoachLocator.deployment" if 'WEBSITE_HOSTNAME' in os.environ else 'CoachLocator.settings'
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CoachLocator.deployment')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
